0a493de389a87a53b8b5494f83b071ac2185eace
nilshah98 <neel12@somaiya.edu>
2019-03-24 00:34:55 +0530

    cli added

--- calcPrimes.js
+++ calcPrimes.js
@@ -1,54 +1,41 @@
-const {Worker, parentPort, workerData} = require('worker_threads');
+const { Worker, parentPort, workerData } = require("worker_threads");
 
+// Get init data from mainThread
 const size = workerData.size;
 const start = workerData.start;
 const limit = workerData.limit;
 var curr = workerData.curr;
 const sieve = workerData.sieve;
-// console.log(sieve,start,size);
-// const sieve = new Array(size).fill(0);
 
-while(curr <= limit && curr <= (start+size-1) ){
-    if(start % curr){
-        var index = curr - (start % curr);
-    }else{
-        var index = 0;
-    }
-    
-    for(index; index<size; index += curr){
-        if((start + index) != curr){
-            sieve[start + index - 2] = 1;
+// While the current factor is less than eq limit
+// While the current factor is less than eq last element of thread
+while (curr <= limit && curr <= start + size - 1) {
+
+    // if sieve isn't marked by anyone else
+    // alter to >=0 to check performance for repeating elements
+    if (sieve[curr - 2] == 0) {
+        // If start is not multiple of curr, index = the extra elems needed
+        if (start % curr) {
+            var index = curr - (start % curr);
+        } else {
+            // if start is multiple of curr, index = 0
+            var index = 0;
+        }
+
+        // loop through each index, incrementing by curr, till size of segment exhausted
+        for (index; index < size; index += curr) {
+            // Not changing if start+index is same as curr
+            if (start + index != curr) {
+                // Else altering flag to 1, as factor found
+                // -2, since 0 indexed and first element is 2 and not 1
+                sieve[start + index - 2] = 1;
+            }
         }
     }
 
-    curr += 1
-
+    // incrementing curr here =>
+    curr += 1;
 }
-// console.log("end",sieve,start);
+
 parentPort.postMessage("done");
-
-// parentPort.on("message",(data) => {
-
-//     if(start % data.curr){
-//         var index = data.curr - (start % data.curr);
-//     }else{
-//         var index = 0;
-//     }
-    
-//     for(index; index<size; index += data.curr){
-//         if((start + index) != data.curr){
-//             data.data[index] = 1;
-//         }
-//     }
-
-//     // console.log(sievex);
-//     // console.log(data.curr);
-//     // console.log(results,data.start);
-//     // parentPort.postMessage(data.data);
-//     // parentPort.close();
-
-//     if(data.curr == limit){
-//         parentPort.close();
-//     }
-// })
-
+parentPort.close();
--- index.js
+++ index.js
@@ -1,126 +1,167 @@
 const os = require("os");
 const path = require("path");
 const { Worker, parentPort, workerData } = require("worker_threads");
+const inquirer = require("inquirer");
+const ora = require("ora");
 
 const cpuCount = os.cpus().length;
 const workerPath = path.resolve("calcPrimes.js");
 
 const calculatePrimes = number => {
-    // constant cannot be changed through re-assignment
-    // constant cannot be changed through re-declaration
-    const numbers = [];
+    return new Promise((resolve, reject) => {
+        // constant cannot be changed through re-assignment
+        // constant cannot be changed through re-declaration
+        const numbers = [];
 
-    const area = new SharedArrayBuffer(number-1);
-    const sieve = new Int8Array(area);
-    for (let i = 2; i <= number; i++) {
-        numbers.push(i);
-    }
-    // console.log(sieve);
-    // ceil so as to distribute the leftout equally among threads
-    const segmentSize = Math.ceil(numbers.length / cpuCount);
-    var segments = [];
+        // Creating a shared array buffer of size => number-1, since 1 is not counted for prime
+        const area = new SharedArrayBuffer(number - 1);
+        const sieve = new Int8Array(area);
 
-    for (let segmentIndex = 0; segmentIndex < cpuCount; segmentIndex++) {
-        const start = segmentIndex * segmentSize;
-        const end = start + segmentSize;
-        const segment = numbers.slice(start, end);
-        segments.push(segment);
-    }
+        // When passing a whole array to worker thread, it makes a copy of the array and no changes made there
+        // Gives a stack overflow error even for calculating upto 1000000 as a copy is made and passed to all threads
+        // const sieve = new Array(number-1).fill(0);
 
-    // console.log(segments);
-    // console.log(numbers);
+        // Get start value and size of each segment, that is passed to the threads
 
-    const limit = Math.ceil(Math.sqrt(number));
+        // ceil so as to distribute the leftout equally among threads
+        const segmentSize = Math.ceil((number - 1) / cpuCount);
+        var segments = [];
 
-    // initialise worker with static global data =>
-    var startTime = Date.now();
-    const workers = segments.map(segment => {
-        return new Promise((resolve, reject) => {
-            var worker = new Worker(workerPath, {
-                workerData: {
-                    start: segment[0],
-                    limit: limit,
-                    size: segment.length,
-                    curr: 2,
-                    sieve: sieve
+        for (let segmentIndex = 0; segmentIndex < cpuCount; segmentIndex++) {
+            const start = segmentIndex * segmentSize;
+            const segment = [start, segmentSize];
+            segments.push(segment);
+        }
+
+        // The limit till which we need to check for primes
+        const limit = Math.ceil(Math.sqrt(number));
+
+        // initialise worker with static global data
+        const workers = segments.map(segment => {
+            // Create a promise for each worker
+            return new Promise((resolve, reject) => {
+                // creating a worker with the some inital workerData
+                const worker = new Worker(workerPath, {
+                    workerData: {
+                        start: segment[0],
+                        size: segment[1],
+                        curr: 2,
+                        limit: limit,
+                        sieve: sieve
+                    }
+                });
+
+                // When worker emits, message | error | exit events
+                worker.on("message", resolve);
+                worker.on("error", reject);
+                worker.on("exit", code => {
+                    if (code !== 0)
+                        reject(
+                            new Error(`Worker stopped with exit code ${code}`)
+                        );
+                });
+            });
+        });
+
+        // When all the workers have completed processing, and returned data in order, and in sync
+        Promise.all(workers).then(res => {
+            // Iterating through shared buffer once, all threads have parsed, and adding elems having 0 to primes
+            let counter = 2;
+            const primes = [];
+            sieve.forEach(elem => {
+                if (elem == 0) {
+                    primes.push(counter);
                 }
+                counter += 1;
             });
-            worker.on("message", resolve);
-            worker.on("error", reject);
-            worker.on("exit", code => {
-                if (code !== 0)
-                    reject(new Error(`Worker stopped with exit code ${code}`));
-            });
+
+            // Logging number of primes, and the time to process
+
+            resolve(primes.length);
         });
     });
-
-    Promise.all(workers).then(res => {
-        let counter = 2;
-        const primes = [];
-        sieve.forEach(elem => {
-            if (!(elem !== 0)) {
-                primes.push(counter);
-            }
-            counter += 1;
-        });
-        console.log(primes.length);
-        console.log(Date.now() - startTime);
-    });
-
-    // var start = 2;
-
-    // const calculate = (workers, segmentss, startTime) => {
-
-    //     var promises = workers.map((worker, index) => {
-    //         return new Promise((resolve, reject) => {
-    //             worker.on("message", resolve);
-    //             worker.on("error", reject);
-    //             worker.on("exit", code => {
-    //                 if (code !== 0)
-    //                     reject(
-    //                         new Error(`Worker stopped with exit code ${code}`)
-    //                     );
-    //             });
-    //         });
-    //     });
-
-    //     workers.forEach((worker, index) => {
-    //         worker.postMessage({
-    //             data: segmentss[index],
-    //             curr: start
-    //         });
-    //     });
-
-    //     Promise.all(promises).then(results => {
-    //         // console.log(results, start);
-    //         start += 1;
-
-    //         if (start <= limit) {
-    //             calculate(workers, results, startTime);
-    //         } else {
-    //             let counter = 2;
-    //             const primes = [];
-    //             results.forEach(res => {
-    //                 res.forEach(elem => {
-    //                     if (elem == 0) {
-    //                         primes.push(counter);
-    //                     }
-    //                     counter += 1;
-    //                 });
-    //             });
-    //             console.log(primes.length);
-    //             console.log(Date.now() - startTime);
-    //         }
-    //     });
-    // };
-
-    // var sieve = [];
-    // segments.forEach(segment => {
-    //     sieve.push(new Array(segment.length).fill(0));
-    // });
-
-    // var startTime = Date.now();
-    // calculate(workers, sieve, startTime);
 };
 
-calculatePrimes(10000000);
+const normalSieve = n => {
+    return new Promise((resolve, reject) => {
+        // Eratosthenes algorithm to find all primes under n
+        var array = [],
+            upperLimit = Math.sqrt(n),
+            output = [];
+
+        // Make an array from 2 to (n - 1)
+        for (var i = 0; i < n; i++) {
+            array.push(true);
+        }
+
+        // Remove multiples of primes starting from 2, 3, 5,...
+        for (var i = 2; i <= upperLimit; i++) {
+            if (array[i]) {
+                for (var j = i * i; j < n; j += i) {
+                    array[j] = false;
+                }
+            }
+        }
+
+        // All array[i] set to true are primes
+        for (var i = 2; i < n; i++) {
+            if (array[i]) {
+                output.push(i);
+            }
+        }
+
+        resolve(output.length);
+    });
+};
+
+const run = async () => {
+    const { primeRange } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "primeRange",
+            message: "Find primes till ?",
+            default: 100
+        }
+    ]);
+    // Written using then
+    // calculatePrimes(primeRange).then((res) => {
+    //     spinner.succeed(`Number of primes : ${res[0]}`)
+    // });
+    const NS_PER_SEC = 1e9;
+
+
+    const spinner = ora("Calculating number of primes-").start();
+    const stTime = process.hrtime();
+    var res = await calculatePrimes(primeRange);
+    const endTime = process.hrtime(stTime);
+    spinner.succeed(`(worker) Number of primes : ${res}`);
+    const time1 = endTime[0] * NS_PER_SEC + endTime[1];
+    spinner.info(
+        `(worker) Benchmark took   : ${time1} nanoseconds`
+    );
+
+    const spinner2 = ora("Calculating number of primes-").start();
+    const stTime2 = process.hrtime();
+    var res = await normalSieve(primeRange);
+    const endTime2 = process.hrtime(stTime2);
+    spinner2.succeed(`(main)   Number of primes : ${res}`);
+    const time2 = endTime2[0] * NS_PER_SEC + endTime2[1];
+    spinner2.info(
+        `(main)   Benchmark took   : ${time2} nanoseconds\n`
+    );
+    spinner2.succeed(`Difference between worker and main : ${(time2 - time1)} nanoseconds`)
+};  
+
+
+run();
+
+/* 
+TODO =>
+0. Clean calculateTime function
+1. Analysis for SharedBufferArray v/s NormalArray
+2. Recurring same process on worker thread after messaging mainThread
+3. Message passing efficiency
+    3.1 Arrays
+    3.2 SharedArrayBuffers
+    3.3 Objects
+*/
